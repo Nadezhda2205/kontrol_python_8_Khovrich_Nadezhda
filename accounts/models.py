987ab0ae8826dd9ddfd634237d050c2844525a66
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-29 09:33:58 +0600

    Отработато login, logout, signup

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from random import choices
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+
+class Account(AbstractUser):
+
+    SEX_CHOICES = [('M', 'мужской'), ('F', 'женский')] 
+    
+    about = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Информация'
+    )
+    phone = models.CharField(
+        null=True,
+        blank=True,
+        verbose_name='Телефон',
+        max_length=15
+
+    )
+    sex = models.CharField(
+        choices=SEX_CHOICES,
+        null=True,
+        blank=True,
+        verbose_name='Пол',
+        max_length=1
+    )
+    
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import authenticate, login, logout
+
+
+from accounts.forms import CustomUserСreationForm
+from accounts.forms import LoginForm
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'accounts/login.html'
+    form = LoginForm
+
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+
+        username: str = form.cleaned_data.get('username')
+        password: str = form.cleaned_data.get('password')
+        user = authenticate(request=request, username=username, password=password)
+
+        if not user:
+            return redirect('login')
+
+        login(request, user)
+
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
+class RegisterView(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = CustomUserСreationForm
+    succes_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
