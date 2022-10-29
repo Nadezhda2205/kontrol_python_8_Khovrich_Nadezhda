778561f8546874f08156e20484d82798c73f23af
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-29 13:15:55 +0600

    Отображение списка продуктов index.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from products.models import Product
+
+
+class ProductListView(ListView):
+    template_name = 'index.html'
+    model = Product
+    context_object_name = 'products'
+
