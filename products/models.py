d1823e3aeb3e6b2eb9847b503281becb09d8e9df
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-29 12:16:14 +0600

    Добавлена модель comment, фикстура

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from accounts.models import AbstractUser
-
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Product(models.Model):
 
@@ -13,15 +13,35 @@ class Product(models.Model):
     ]
 
     name = models.CharField(verbose_name='Наименование', max_length=100, null=False)
-    description = models.TextField(verbose_name='Описание', max_length=2000, null=True, blank=True)
+    description = models.TextField(verbose_name='Описание', max_length=2000, null=False)
     category = models.CharField(
         verbose_name='Категория', 
         max_length=10, 
         choices=CATEGORY_CHOICES, 
-        default='other'
+        default='other',
+        null=False,
         )
-    image = models.CharField(verbose_name='Категория', default='image', null=True)
+    image = models.TextField(verbose_name='Изображение', default='image', null=True)
 
 
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('one', '1'),
+        ('two', '2'),
+        ('three', '3'),
+        ('four', '4'),
+        ('five', '5'),
+    ]
+    author = models.ForeignKey(verbose_name='Автор', to=get_user_model(), related_name='comments', null=False,
+                               blank=False,
+                               on_delete=models.CASCADE)
+    product = models.ForeignKey(verbose_name='Продукт', to=Product, related_name='comments', null=False,
+                             blank=False, on_delete=models.CASCADE)
+    text = models.CharField(verbose_name='Текст', null=False, blank=False, max_length=500)
+    created_at = models.DateTimeField(auto_now_add = True)
+    valuation = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+
