3d11bdf008946ca11eabbfaea017abcbfcc8f45c
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-29 11:48:03 +0600

    Создана модель products, миграция.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import AbstractUser
 
-# Create your models here.
+
+class Product(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('milk', 'Молочные'),
+        ('meat', 'Мясные'),
+        ('bread', 'Хлебобулочные'),
+        ('juice', 'Соки'),
+        ('other', 'Разное'),
+    ]
+
+    name = models.CharField(verbose_name='Наименование', max_length=100, null=False)
+    description = models.TextField(verbose_name='Описание', max_length=2000, null=True, blank=True)
+    category = models.CharField(
+        verbose_name='Категория', 
+        max_length=10, 
+        choices=CATEGORY_CHOICES, 
+        default='other'
+        )
+    image = models.CharField(verbose_name='Категория', default='image', null=True)
+
+
+    def __str__(self):
+        return self.name
